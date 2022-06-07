4c49b985c0ae8854ad78752234d627b0ad3506a3
mohitjain41 <43922177+mohitjain41@users.noreply.github.com>
2022-06-07 15:20:41 +0530

    Add files via upload

---
+++ ProductRepository.java
@@ -0,0 +1,9 @@
+package com.assignment;
+
+public class ProductRepository {
+    static Product[] arr= {new Product(1,"phone",45000.0,"smartphone"),
+            new Product(1,"T-shirts",450.0,"Wearable"),
+            new Product(2, "crocs",600 , "Wearable"),
+            new Product(3, "burger", 90.0, "eatable"),
+            new Product(4, "tacos", 80.0, "eatable")};
+}
---
+++ ProjectServices.java
@@ -0,0 +1,45 @@
+package com.assignment;
+
+import java.util.ArrayList;
+
+public class ProjectServices {
+    public static String findNameByCode(int num) {
+        for (int i = 0; i < ProductRepository.arr.length; i++) {
+            if (ProductRepository.arr[i].getProductCode() == num) {
+                return ProductRepository.arr[i].getName();
+            }
+        }
+        return "null";
+    }
+    public static Product findMaxPrice(String category){
+        double maxPrice = Double.MIN_VALUE;
+        int index = -1;
+        for(int i=0;i<ProductRepository.arr.length;i++){
+            if(ProductRepository.arr[i].getCategory().equals(category)){
+                maxPrice = ProductRepository.arr[i].getPrice();
+                index = i;
+            }
+        }
+        if(index == -1){
+            return null;
+        }else{
+            return ProductRepository.arr[index];
+        }
+    }
+    public static Product[] getProductsByCategory(String category) {
+        ArrayList<Product> list=new ArrayList<Product>();
+
+        for(int i=0; i<ProductRepository.arr.length; i++) {
+            if(ProductRepository.arr[i].getCategory().equals(category)) {
+                list.add(ProductRepository.arr[i]);
+            }
+        }
+        if(list.isEmpty()) {
+            return null;
+        }
+        else {
+            Product[] arr=list.toArray(new Product[list.size()]);
+            return arr;
+        }
+    }
+}
